45399d58536ac33da25d67ff366a1a23a43081a0
spencerhh <spencer.hall16@gmail.com>
2018-12-09 01:04:29 -0800

    Added basic error handling for missing search parameters. Changed the related artists section so that it updates the page with information about the selected artist. Added more structure to the Songkick API section.

--- index.js
+++ index.js
@@ -5,18 +5,16 @@
      * 3. Overall design updates
      * 4. Songkick API implementation
      * 5. Update comments
-     * 6. Content section header title based on returned data
-     * 7. Mathematical breakdown of lyrics or something interesting
-     * 8. 404 page
-     * 9. Check if works without JS
-     * 10. Sitemap
-     * 11. Some sort of styling for total error
+     * 6. Mathematical breakdown of lyrics or something interesting
+     * 7. Some sort of styling for total error
      */
 
     "use strict";
 
     let lyricsSuccess = true;
     let tasteDiveSuccess = true;
+
+    const inputIDs = ["artist-name", "song-title"];
 
     // Base URLs and keys for APIs
     const API_URLS = {
@@ -26,9 +24,8 @@
     };
     const API_KEYS = {
         tastedive: "324293-lyrickr-KFTB6MHP",
-        songkick: "WT5YY3PAVZEiiiqv",
+        songkick: "WT5YY3PAVZEiiiqv"
     };
-
 
     window.addEventListener("load", initialize);
 
@@ -39,21 +36,14 @@
     function initialize() {
         $("search").addEventListener("click", search);
 
-        document.getElementById("artist-name")
-            .addEventListener("keyup", function(event) {
+        inputIDs.forEach(function(id) {
+            $(id).addEventListener("keyup", function(event) {
                 event.preventDefault();
                 if (event.keyCode === 13) { // Code for the "enter" key
-                    document.getElementById("search").click();
+                    $("search").click();
                 }
             });
-
-        document.getElementById("song-title")
-            .addEventListener("keyup", function(event) {
-                event.preventDefault();
-                if (event.keyCode === 13) {
-                    document.getElementById("search").click();
-                }
-            });
+        });
     }
 
 
@@ -64,10 +54,19 @@
     function search() {
         killTheChildren();
 
-        let artistName = $("artist-name").value;
-        let songTitle = $("song-title").value;
+        let artistName = val("artist-name");
+        let songTitle = val("song-title");
 
-        retrieveLyricsData(artistName, songTitle); // Uses the lyrics.ovh API
+        if (artistName == "" && songTitle == "") { // No forms are filled
+            alert("Please fill in something to search.");
+        } else if (artistName == "" && songTitle != "") {
+            alert("Please enter an artist.");
+        } else if (artistName != "" && songTitle == "") {
+            lyricsSuccess = true;
+            retrieveTasteDiveData(artistName);
+        } else { // Forms are properly filled
+            retrieveLyricsData(artistName, songTitle); // Uses the lyrics.ovh API
+        }
     }
 
 
@@ -75,21 +74,21 @@
 
     /* ------------------------------- Error Handling ------------------------------- */
 
-
     /**
      * Displays an error message for the user if all API retrievals fail and hides
      * the error message if the user clicks back into the input boxes.
      */
     function handleTotalError() {
         // Possibly delete children? Saw an error with two error messages because clicks were in quick succession
-        $("content-section").classList.remove("hidden");
+
+        toggle("content-section", "hidden");
         $("content-header").innerHTML = "sorry";
 
         killTheChildren();
 
-        let errorDiv = document.createElement("div");
+        let errorDiv = ce("div");
         errorDiv.classList.add("content-child");
-        let errorMessage = document.createElement("p");
+        let errorMessage = ce("p");
         errorMessage.innerHTML = "Unfortunately, we could not find any information for your request. Check your search and try again!";
         errorDiv.append(errorMessage);
         $("content-container").append(errorDiv);
@@ -103,7 +102,7 @@
      * Resets the content section after a total error has occurred.
      */
     function hideContentSection() {
-        $("content-section").classList.add("hidden");
+        toggle("content-section", "hidden");
         $("content-header").innerHTML = "retrieved data";
 
         $("artist-name").removeEventListener("click", hideContentSection);
@@ -114,7 +113,6 @@
 
 
     /* ------------------------------- Lyrics.ovh API ------------------------------- */
-
 
     /**
      * Makes an AJAX call using the artist's name and song title. Success and failure
@@ -143,14 +141,14 @@
         lyricsSuccess = true; // Resets this value after possible previous total errors
 
         let lyricsData = responseData.lyrics;
-        let lyrics = document.createElement("pre");
+        let lyrics = ce("pre");
         lyrics.classList.add("content-child");
         lyrics.innerHTML = lyricsData;
 
-        $("content-section").classList.remove("hidden");
+        toggle("content-section", "hidden");
         $("content-container").append(lyrics);
 
-        retrieveTasteDiveData($("artist-name").value); // Uses the TasteDive API
+        retrieveTasteDiveData(val("artist-name")); // Uses the TasteDive API
     }
 
 
@@ -160,7 +158,7 @@
      */
     function handleLyricsError() {
         lyricsSuccess = false;
-        retrieveTasteDiveData($("artist-name").value);
+        retrieveTasteDiveData(val("artist-name"));
     }
 
 
@@ -168,9 +166,8 @@
 
     /* -------------------------------- TasteDive API -------------------------------- */
 
-
     /**
-     * Uses jQuery to request data about the user's specified artist. If this and the 
+     * Uses jQuery to request data about the user's specified artist. If this and the
      * lyrics.ovh call failed, it will invoke the handleTotalError function. Success
      * of this function will display its data.
      * @param {string} artistName - artist name from the user
@@ -190,11 +187,7 @@
             let result = data.Similar;
             if (result.Info[0].Type == "unknown") {
                 tasteDiveSuccess = false;
-                if (lyricsSuccess == false) {
-                    handleTotalError();
-                } else {
-                    retrieveSongkickData(artistName);
-                }
+                searchSongkick(artistName);
             } else {
                 tasteDiveSuccess = true;
                 prepareTasteDiveData(result);
@@ -204,7 +197,7 @@
 
 
     /**
-     * Creates an object of elements ready to be appended to the document using the 
+     * Creates an object of elements ready to be appended to the document using the
      * data returned from the TasteDive API. Passes this object to displayTasteDiveData
      * to be displayed.
      * @param {JSON} data - JSON data from TasteDive API
@@ -215,32 +208,37 @@
         let artistBio = data.Info[0].wTeaser;
         let artistWiki = data.Info[0].wUrl;
 
-        let artistWikiLink = document.createElement("a");
+        let artistWikiLink = ce("a");
         artistWikiLink.href = artistWiki;
         artistWikiLink.innerHTML = "Wikipedia";
         artistWikiLink.target = "blank";
 
-        let artistBioP = document.createElement("p");
+        let artistBioP = ce("p");
         artistBioP.innerHTML = artistBio;
-        artistBioP.append(document.createElement("br"));
-        artistBioP.append(document.createElement("br"));
+        artistBioP.append(ce("br"));
+        artistBioP.append(ce("br"));
         artistBioP.append(artistWikiLink);
 
-        let artistBioDiv = document.createElement("div");
+        let artistBioDiv = ce("div");
         artistBioDiv.classList.add("content-child");
         artistBioDiv.append(artistBioP);
         tasteDiveData[0] = artistBioDiv;
 
         // Stores individual <li> elements in the rest of the tasteDiveData object
         for (let i = 0; i < data.Results.length; i++) {
-            let relatedArtist = document.createElement("li");
+            let relatedArtist = ce("li");
 
-            let relatedArtistLink = document.createElement("a");
-            relatedArtistLink.href = data.Results[i].wUrl;
-            relatedArtistLink.target = "blank";
+            let relatedArtistLink = ce("p");
+            relatedArtistLink.classList.add("artist");
             relatedArtistLink.innerHTML = data.Results[i].Name;
-            relatedArtist.append(relatedArtistLink);
 
+            relatedArtistLink.addEventListener("click", function() {
+                $("artist-name").value = this.innerHTML;
+                $("song-title").value = "";
+                search();
+            });
+
+            relatedArtist.append(relatedArtistLink);
             tasteDiveData[i + 1] = relatedArtist;
         }
 
@@ -249,29 +247,29 @@
 
 
     /**
-     * Prepares the content section for display and then appends the content-container with 
+     * Prepares the content section for display and then appends the content-container with
      * the elements containg the TasteDive data.
      * @param {object[]} data - object full of elements with TasteDive data
      */
     function displayTasteDiveData(data) {
         // Possibly needs a case for artist's with no bio, but related artists
         if ($("content-section").classList.contains("hidden")) {
-            $("content-section").classList.remove("hidden");
+            toggle("content-section", "hidden");
         }
         $("content-header").innerHTML = "retrieved data";
 
         $("content-container").append(data[0]); // Artist's bio
 
         if (data[1] != undefined) { // Handles case for artists without related artists, but have bios
-            let sectionTitle = document.createElement("p");
+            let sectionTitle = ce("p");
             sectionTitle.innerHTML = "Related Artists";
             sectionTitle.id = "related-artists";
 
-            let relatedArtistsSection = document.createElement("div");
+            let relatedArtistsSection = ce("div");
             relatedArtistsSection.classList.add("content-child");
             relatedArtistsSection.append(sectionTitle);
 
-            let relatedArtistsList = document.createElement("ul");
+            let relatedArtistsList = ce("ul");
             for (let i = 1; i < 11; i++) {
                 relatedArtistsList.append(data[i]);
             }
@@ -279,7 +277,8 @@
             relatedArtistsSection.append(relatedArtistsList);
             $("content-container").append(relatedArtistsSection);
         }
-        retrieveSongkickData($("artist-name").value);
+
+        searchSongkick(val("artist-name"));
     }
 
 
@@ -287,12 +286,11 @@
 
     /* -------------------------------- Songkick API -------------------------------- */
 
-
     /**
      * ...
      * @param {type} name - description
      */
-    function retrieveSongkickData(artistName) {
+    function searchSongkick(artistName) {
         let url = API_URLS.songkick + API_KEYS.songkick + "&query=" + artistName;
 
         fetch(url, {
@@ -300,8 +298,8 @@
             })
             .then(checkStatus)
             .then(JSON.parse)
-            .then(displaySongkickData)
-            .catch(handleSongkickError);
+            .then(handleSongkickResponse)
+            .catch(handleSongkickError); // Error will only occur if can't connect to URL
     }
 
 
@@ -309,10 +307,19 @@
      * ...
      * @param {type} name - description
      */
-    function displaySongkickData(responseData) {
+    function handleSongkickResponse(responseData) {
         console.log(responseData);
-        if (responseData.resultsPage.status == "ok") {
+        if (responseData.resultsPage.totalEntries > 0) {
             console.log("And the value we actually want (artist id) is: " + responseData.resultsPage.results.artist[0].id);
+            // Without another API call we can see the artist's id (for more API calls), his Songkick page, and how long he is on tour for
+
+            retrieveSongkickData(responseData.resultsPage.results.artist[0].id);
+
+
+
+
+        } else {
+            handleSongkickError(); // If there isn't data, then we can treat it as an error
         }
     }
 
@@ -324,12 +331,12 @@
     function handleSongkickError(error) {
         if (!(lyricsSuccess && tasteDiveSuccess)) {
             handleTotalError();
-        } else {
-            console.log("Songkick didn't work, but at least something did.");
         }
     }
 
-
+    function retrieveSongkickData(artistID) {
+        console.log(artistID);
+    }
 
 
     /* ------------------------------- Musixmatch API ------------------------------- */
@@ -340,7 +347,7 @@
     /* ------------------------------ Helper Functions ------------------------------ */
 
     /**
-     * ...
+     * Kills all of the child elements of the content-container div.
      */
     function killTheChildren() {
         let contentChildren = qsa(".content-child");
@@ -366,6 +373,45 @@
     function qsa(query) {
         return document.querySelectorAll(query);
     }
+
+    /**
+     * Creates and returns a DOM object with the specified tag.
+     * @param {string} e - tag of the DOM object to create
+     * @returns {DOM object} a newly created DOM object with the given tag.
+     */
+    function ce(el) {
+        return document.createElement(el);
+    }
+
+
+
+
+    /**
+     * Creates and returns a DOM object with the specified tag.
+     * @param {string} e - tag of the DOM object to create
+     * @returns {DOM object} a newly created DOM object with the given tag.
+     */
+    function val(id) {
+        return $(id).value;
+    }
+
+
+
+
+    /**
+     * Hides the given DOM element by adding the "hidden" class to the associated DOM.
+     * @param {string} id of a DOM element
+     */
+    function toggle(id, cl) {
+        if ($(id).classList.contains(cl)) {
+            $(id).classList.remove(cl);
+        } else {
+            $(id).classList.add(cl);
+        }
+    }
+
+
+
 
     /*
      * Helper function to return the response's result text if successful, otherwise
